b19eabec3526efc0e63f2bb3f8ce4ce1d1a7abde
Tobias Kaiser <mail@tb-kaiser.de>
2022-01-09 18:19:39 +0100

    working on json schema based validation

--- passmate/raw_db.py
+++ passmate/raw_db.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 import json
+import jsonschema
+
+class DatabaseException(Exception):
+    pass
 
 @dataclass(frozen=True)
 class FieldTuple:
@@ -22,14 +26,58 @@ class RawDatabaseJSONEncoder(json.JSONEncoder):
             return json.JSONEncoder.default(self, o)
 
 class RawDatabase:
+
+    json_schema = {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "title": "Passmate database schema",
+        "type": "object",
+        "properties": {
+            "purpose": {
+                "description": "Distinguish between primary database and sync copy",
+                "type": "string"
+            }
+        }, 
+        "required": ["purpose", "records", "version"]
+    }
+
     def __init__(self):
-        self.purpose = "primary"
+        self.is_sync_copy = False
         self.records = {}
 
-    def json(self):
+    def json(self, purpose="primary"):
+        if self.is_sync_copy:
+            raise DatabaseException("json() called on sync copy")
+
+        assert purpose in ("primary", "sync_copy")
         obj = {
             "version": 2,
-            "purpose": self.purpose,
+            "purpose": purpose,
             "records": self.records
         }
         return RawDatabaseJSONEncoder().encode(obj)
+
+    @classmethod
+    def from_json(cls, json_str):
+        obj = json.loads(json_str)
+
+        jsonschema.validate(instance=obj, schema=cls.json_schema)
+
+        db = RawDatabase()
+        
+        assert obj["version"] == 2
+        # TODO: Replace this with JSON schema checking
+        if obj["purpose"] == "sync_copy":
+            db.is_sync_copy = True
+
+        for record_id, field_tuples_json in obj["records"].items():
+            record = RawRecord()
+            for (domain, field_name, field_value, mtime) in field_tuples_json:
+                field_tuple = FieldTuple(domain, field_name, field_value, mtime)
+                record.append(field_tuple)
+            assert not record_id in db.records
+            db.records[record_id] = record
+
+        return db
+
+    def __repr__(self):
+        return f"RawDatabase(is_sync_copy={self.is_sync_copy}, records={self.records})"
--- tests/test_raw_db.py
+++ tests/test_raw_db.py
@@ -1,7 +1,12 @@
-from passmate.raw_db import RawDatabase, RawRecord, FieldTuple
+import pytest
 import json
+import jsonschema
 
-def test_encode_json():
+from passmate.raw_db import RawDatabase, RawRecord, FieldTuple, DatabaseException
+
+
+@pytest.mark.parametrize("purpose", ["primary", "sync_copy"])
+def test_encode_json(purpose):
     db = RawDatabase()
     db.records["RecordA"] = RawRecord([
         FieldTuple("user", "email", "invalid@example.com", 123),
@@ -11,7 +16,7 @@ def test_encode_json():
 
     expected_obj = {
         "version": 2,
-        "purpose": "primary",
+        "purpose": purpose,
         "records": {
             "RecordA":[
                 ["user", "email", "invalid@example.com", 123],
@@ -20,6 +25,49 @@ def test_encode_json():
             ]
         }
     }
-    generated_obj = json.loads(db.json())
-
+    generated_obj = json.loads(db.json(purpose=purpose))
     assert generated_obj == expected_obj
+
+@pytest.mark.parametrize("purpose", ["primary", "sync_copy"])
+def test_decode_json(purpose):
+    input_json_obj = {
+        "version": 2,
+        "purpose": purpose,
+        "records": {
+            "RecordA":[
+                ["user", "email", "invalid@example.com", 123],
+                ["user", "password", "abcd", 124],
+                ["user", "password", "xyz", 5678]
+            ],
+            "RecordB":[
+                ["meta", "path", "MyTestPath", 123456],
+            ]
+        }
+    }
+    input_json = json.dumps(input_json_obj)
+    db = RawDatabase.from_json(input_json)
+
+    if purpose == "primary":
+        assert not db.is_sync_copy
+        generated_obj = json.loads(db.json())
+        assert generated_obj == input_json_obj
+    elif purpose == "sync_copy":
+        # If we load a sync copy, it should not be possible to save it again as JSON
+        assert db.is_sync_copy
+        with pytest.raises(DatabaseException):
+            db.json()
+    else:
+        assert False
+
+
+invalid_json_objs = [
+    {},
+    {"version": 2, "purpose": "blabla", "records": {}},
+    {"version": 3, "purpose": "blabla", "records": {}},
+]
+
+@pytest.mark.parametrize("obj", invalid_json_objs)
+def test_invalid_json(obj):
+    obj_json = json.dumps(obj)
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        db = RawDatabase.from_json(obj_json)
