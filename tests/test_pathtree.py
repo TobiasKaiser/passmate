51bbc4a02719f0f51c4abc81ee16fe2dd870b600
Tobias Kaiser <mail@tb-kaiser.de>
2022-08-26 14:24:56 +0200

    test_pathtree.py refactored

--- tests/test_pathtree.py
+++ tests/test_pathtree.py
@@ -3,107 +3,142 @@ import pytest
 from .start_session import start_session
 from passmate.session import SessionException, Record
 from passmate.raw_db import RawDatabase, RawDatabaseUpdate, FieldTuple
+import copy
+
+def sample_session_populate(session):
+    session["record1"] = Record()
+    session["path1/Record2"] = Record()
+    session["path1/example3"] = Record()
+    session["path2/x/y/z/abc/record4"] = Record()
+
+sample_session_expected_tree = [
+    "|",
+    "+-> path1/",
+    "| +-- Record2",
+    "| +-- example3",
+    "+-> path2/",
+    "| +-> x/",
+    "|   +-> y/",
+    "|     +-> z/",
+    "|       +-> abc/",
+    "|         +-- record4",
+    "+-- record1",
+]
 
 def test_pathtree(tmp_path):
     with start_session(tmp_path, init=True) as session:
-        session.current_time = 2000
-        session["record1"] = Record()
-        session["path1/record2"] = Record()
-        session["path1/record3"] = Record()
-        session["path2/x/y/z/abc/record4"] = Record()
 
-        expected_tree_str1 = "\n".join([
-            "|",
-            "+-> path1/",
-            "| +-- record2",
-            "| +-- record3",
-            "+-> path2/",
-            "| +-> x/",
-            "|   +-> y/",
-            "|     +-> z/",
-            "|       +-> abc/",
-            "|         +-- record4",
-            "+-- record1",
-        ])
+        sample_session_populate(session)
+        
+        expected_tree = copy.copy(sample_session_expected_tree)
 
         assert session.tree.reload_counter == 0
-        assert session.tree.tree_str() == expected_tree_str1
+        assert session.tree.tree_str() == "\n".join(expected_tree)
         assert session.tree.reload_counter == 1
 
-        # Operation 1: A field update should not cause a tree update.
+def test_pathtree_field_update(tmp_path):
+    with start_session(tmp_path, init=True) as session:
 
-        session["path1/record2"]["my_field"] = "myValue"
-        assert session.tree.tree_str() == expected_tree_str1
+        sample_session_populate(session)
+
+        expected_tree = copy.copy(sample_session_expected_tree)
+
+        assert session.tree.reload_counter == 0
+        assert session.tree.tree_str() == "\n".join(expected_tree)
         assert session.tree.reload_counter == 1
 
-        # Operation 2: Creation of a new record should trigger a tree update.
+        # A field update should not cause a tree update:
+
+        session["path1/Record2"]["my_field"] = "myValue"
+        assert session.tree.tree_str() == "\n".join(expected_tree)
+        assert session.tree.reload_counter == 1
+
+def test_pathtree_invalidate_new_record(tmp_path):
+    with start_session(tmp_path, init=True) as session:
+
+        sample_session_populate(session)
+
+        expected_tree = copy.copy(sample_session_expected_tree)
+
+        assert session.tree.reload_counter == 0
+        assert session.tree.tree_str() == "\n".join(expected_tree)
+        assert session.tree.reload_counter == 1
+
+        # Creation of a new record should trigger a tree update.
 
         session["path1/record5"] = Record()
 
-        expected_tree_str2 = "\n".join([
-            "|",
-            "+-> path1/",
-            "| +-- record2",
-            "| +-- record3",
-            "| +-- record5",
-            "+-> path2/",
-            "| +-> x/",
-            "|   +-> y/",
-            "|     +-> z/",
-            "|       +-> abc/",
-            "|         +-- record4",
-            "+-- record1",
-        ])
+        expected_tree.insert(4, "| +-- record5")
 
-        assert session.tree.tree_str() == expected_tree_str2
+        assert session.tree.tree_str() == "\n".join(expected_tree)
         assert session.tree.reload_counter == 2
 
-        # Operation 3: Deletion of a record should trigger a tree update.
+def test_pathtree_invalidate_delete(tmp_path):
+    with start_session(tmp_path, init=True) as session:
+
+        sample_session_populate(session)
+
+        expected_tree = copy.copy(sample_session_expected_tree)
+
+        assert session.tree.reload_counter == 0
+        assert session.tree.tree_str() == "\n".join(expected_tree)
+        assert session.tree.reload_counter == 1
+
+        # Deletion of a record should trigger a tree update:
 
         del session["path2/x/y/z/abc/record4"]
 
-        expected_tree_str3 = "\n".join([
-            "|",
-            "+-> path1/",
-            "| +-- record2",
-            "| +-- record3",
-            "| +-- record5",
-            "+-- record1",
-        ])
+        for line in [
+                "+-> path2/",
+                "| +-> x/",
+                "|   +-> y/",
+                "|     +-> z/",
+                "|       +-> abc/",
+                "|         +-- record4"]:
+            expected_tree.remove(line)
 
-        assert session.tree.tree_str() == expected_tree_str3
-        assert session.tree.reload_counter == 3
+        assert session.tree.tree_str() == "\n".join(expected_tree)
+        assert session.tree.reload_counter == 2
 
-        # Operation 4: Renaming a record should trigger a tree update.
+def test_pathtree_invalidate_rename(tmp_path):
+    with start_session(tmp_path, init=True) as session:
 
-        session["renamed_record"] = session["path1/record2"]
+        sample_session_populate(session)
 
-        expected_tree_str4 = "\n".join([
-            "|",
-            "+-> path1/",
-            "| +-- record3",
-            "| +-- record5",
-            "+-- record1",
-            "+-- renamed_record",
-        ])
+        expected_tree = copy.copy(sample_session_expected_tree)
 
-        assert session.tree.tree_str() == expected_tree_str4
-        assert session.tree.reload_counter == 4
+        assert session.tree.reload_counter == 0
+        assert session.tree.tree_str() == "\n".join(expected_tree)
+        assert session.tree.reload_counter == 1
+    
+        # Renaming a record should trigger a tree update.
+
+        session["renamed_record"] = session["path1/Record2"]
+
+        expected_tree.remove("| +-- Record2",)
+        expected_tree.append("+-- renamed_record")
+
+        assert session.tree.tree_str() == "\n".join(expected_tree)
+        assert session.tree.reload_counter == 2
+
+def test_pathtree_invalidate_merge(tmp_path):
+    with start_session(tmp_path, init=True) as session:
+
+        sample_session_populate(session)
+
+        expected_tree = copy.copy(sample_session_expected_tree)
+
+        assert session.tree.reload_counter == 0
+        assert session.tree.tree_str() == "\n".join(expected_tree)
+        assert session.tree.reload_counter == 1
+    
 
         # Operation 5: A merge should trigger a tree update.
 
         merge_in = RawDatabase()
         merge_in.update(RawDatabaseUpdate('RecordA', FieldTuple('meta', 'path', 'HelloWorld', 100)))
         session.merge(merge_in)
+        expected_tree.append("+-- HelloWorld")
 
-        expected_tree_str5 = "\n".join([
-            "|",
-            "+-> path1/",
-            "| +-- record3",
-            "| +-- record5",
-            "+-- record1",
-            "+-- renamed_record",
-            "+-- HelloWorld",
-        ])
-        assert session.tree.tree_str() == expected_tree_str5
-        assert session.tree.reload_counter == 5
+        assert session.tree.tree_str() == "\n".join(expected_tree)
+        assert session.tree.reload_counter == 2
